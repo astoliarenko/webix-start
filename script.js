48c7713529935e8288fe0f7c3dfad73e91adb600
Aliaksei <dekomessi@mail.ru>
2021-09-26 17:42:58 +0300

    beatify and prettier were used

--- script.js
+++ script.js
@@ -1,178 +1,197 @@
 const label = {
-    view: "label",
-    id: "header-label",
-    label: "My App",
-}
+  view: "label",
+  id: "header-label",
+  label: "My App",
+};
 
 const mainListLabel = {
-    view: "label",
-    id: "main-list-label",
-    label: "<span class='webix_icon wxi-check'></span> Connected",
-    css: "main-list-label-style",
-}
+  view: "label",
+  id: "main-list-label",
+  label: "<span class='webix_icon wxi-check'></span> Connected",
+  css: "main-list-label-style",
+};
 
 const button = {
-    view: "button",
-    id: "header-button",
-    type: "icon",
-    icon: "mdi mdi-account",
-    label: "Profile",
-    width: 150,
-    css: "webix_transparent",
-    click: () => {
-        $$("myPopup").show($$("header-button").getNode());
-        // $$("myPopup").show();
-    }
-}
+  view: "button",
+  id: "header-button",
+  type: "icon",
+  icon: "mdi mdi-account",
+  label: "Profile",
+  width: 150,
+  css: "webix_transparent",
+  click: () => {
+    $$("myPopup").show($$("header-button").getNode());
+  },
+};
 
 const list = {
-    view: "list",
-    id: "main-list",
-    width: 250,
-    template: "#title#",
-    select: true,
-    scroll: false,
-    css: "main-list-style",
-    data: [
-      { id:1, title:"Dashboard" },
-      { id:2, title:"Users" },
-      { id:3, title:"Products" },
-      { id:4, title:"Locations" },
-    ]
-  };
+  view: "list",
+  id: "main-list",
+  width: 250,
+  template: "#title#",
+  select: true,
+  scroll: false,
+  css: "main-list-style",
+  data: [
+    { id: 1, title: "Dashboard" },
+    { id: 2, title: "Users" },
+    { id: 3, title: "Products" },
+    { id: 4, title: "Locations" },
+  ],
+};
 
 const datatable = {
-    view: "datatable",
-    id: "main-datatable",
-    data: small_film_set,
-    autoConfig: true,
-    scrollX: false,
-}
+  view: "datatable",
+  id: "main-datatable",
+  data: small_film_set,
+  autoConfig: true,
+  scrollX: false,
+};
 
 const form = {
-    width: 300,
-    view: "form",
-    id: "main-form",
-    elements: [
-        { view:"template", template: "edit films", type:"section" },
+  width: 300,
+  view: "form",
+  id: "main-form",
+  elements: [
+    { view: "template", template: "edit films", type: "section" },
+    {
+      margin: 10,
+      rows: [
         {
-            margin: 10,
-            rows: [
-                { view:"text", label:"Title", id: "inpTitle", name: "title", invalidMessage: "Title must not be empty" },
-                { view:"text", label:"Year", id: "inpYear", name: "year", invalidMessage: "Ent. year between 1970 and cur."},
-                { view:"text", label:"Rating", id: "inpRating", name: "rating", invalidMessage: "Cannot be empty or 0"},
-                { view:"text", label:"Votes", id: "inpVotes", name: "votes", invalidMessage: "Votes must be less than 100000"},
-                {   
-                    margin: 10,
-                    cols: [
-                        {
-                            view: "button",
-                            value: "Add new",
-                            css: "webix_primary",
-                            click: addItem
-                        },
-                        {
-                            view: "button",
-                            value: "Clear",
-                            click: clearForm
-                        }
-                    ]
-                }
-            ]
+          view: "text",
+          label: "Title",
+          id: "inpTitle",
+          name: "title",
+          invalidMessage: "Title must not be empty",
         },
-        {}
-    ],
-    rules: {
-        title: webix.rules.isNotEmpty,
-        year: (value) => {
-            return (value > 1970 && value <= new Date().getFullYear());
+        {
+          view: "text",
+          label: "Year",
+          id: "inpYear",
+          name: "year",
+          invalidMessage: "Ent. year between 1970 and cur.",
         },
-        votes: (value) => {
-            return value < 100000;
+        {
+          view: "text",
+          label: "Rating",
+          id: "inpRating",
+          name: "rating",
+          invalidMessage: "Cannot be empty or 0",
         },
-        rating: (value) => {
-            return (value !== "" && value !== 0);
-        }
+        {
+          view: "text",
+          label: "Votes",
+          id: "inpVotes",
+          name: "votes",
+          invalidMessage: "Votes must be less than 100000",
+        },
+        {
+          margin: 10,
+          cols: [
+            {
+              view: "button",
+              value: "Add new",
+              css: "webix_primary",
+              click: addItem,
+            },
+            {
+              view: "button",
+              value: "Clear",
+              click: clearForm,
+            },
+          ],
+        },
+      ],
     },
-    on: {
-        onValidationError:function(key){
-            webix.message({text: `${key} field is incorrect`, type:"error"});
-        }
-    }
-}
-
-const mainFormId = "main-form";
-const widthPopUp = 300;
-const paddingXToolbar = 12;
+    {},
+  ],
+  rules: {
+    title: webix.rules.isNotEmpty,
+    year: (value) => {
+      return value > 1970 && value <= new Date().getFullYear();
+    },
+    votes: (value) => {
+      return value < 100000;
+    },
+    rating: (value) => {
+      return value !== "" && value !== 0;
+    },
+  },
+  on: {
+    onValidationError: function (key) {
+      webix.message({ text: `${key} field is incorrect`, type: "error" });
+    },
+  },
+};
 
 function clearForm() {
-    webix.confirm("Clear the form?")
-    .then( () => {
-        $$(mainFormId).clear();
-        $$(mainFormId).clearValidation();
-    });
+  webix.confirm("Clear the form?").then(() => {
+    $$(mainFormId).clear();
+    $$(mainFormId).clearValidation();
+  });
 }
 
 function addItem() {
-    let res = $$(mainFormId).validate();
-    if (res) {
-        let itemData = $$(mainFormId).getValues();
-        webix.message({text: "film info was added to table", type:"success"});
-        $$("main-datatable").add(itemData);
-        $$(mainFormId).clear();
-    }
+  let res = mainForm.validate();
+  if (res) {
+    let itemData = mainForm.getValues();
+    webix.message({ text: "film info was added to table", type: "success" });
+    $$("main-datatable").add(itemData);
+    mainForm.clear();
+  }
 }
-
+// webix.ready( function() {
 webix.ui({
-    view:"popup",
-    id:"myPopup",
-    width: widthPopUp,
-    head:"My window",
-    body:{
-        view: "list",
-        id: "pop-list",
-        autoheight: true,
-        template: "#title#",
-        select: true,
-        scroll: false,
-        data: [
-            { id:1, title:"Settings" },
-            { id:2, title:"Log Out" },
-        ]
-    }
+  view: "popup",
+  id: "myPopup",
+  width: 300,
+  head: "My window",
+  body: {
+    view: "list",
+    id: "pop-list",
+    autoheight: true,
+    template: "#title#",
+    select: true,
+    scroll: false,
+    data: [
+      { id: 1, title: "Settings" },
+      { id: 2, title: "Log Out" },
+    ],
+  },
 });
 
 webix.ui({
-    rows: [
+  rows: [
+    {
+      view: "toolbar",
+      id: "header",
+      paddingX: 12,
+      css: "webix_dark",
+      cols: [label, button],
+    },
+    {
+      id: "main",
+      cols: [
         {
-            view: "toolbar",
-            id: "header",
-            paddingX: paddingXToolbar,
-            css: "webix_dark",
-            cols: [label, button]
+          // css: "menu",
+          rows: [list, mainListLabel],
         },
-        {
-            id: "main",
-            cols: [
-            {
-                // css: "menu",
-                rows: [
-                    list,
-                    mainListLabel
-                ]
-            },
-                {view: "resizer"}, 
-                datatable,
-                form
-            ]
-        },
-        {   
-            id: "footer",
-            view: "template",
-            template: "The software is provided by <a href='https://webix.com'>https://webix.com</a>. All rights reserved (c)",
-            autoheight: true,
-            css: "footer-style",
-            align: "center"
-        }
-    ]
+        { view: "resizer" },
+        datatable,
+        form,
+      ],
+    },
+    {
+      id: "footer",
+      view: "template",
+      template:
+        "The software is provided by <a href='https://webix.com'>https://webix.com</a>. All rights reserved (c)",
+      autoheight: true,
+      css: "footer-style",
+      align: "center",
+    },
+  ],
 });
+// });
+
+const mainForm = $$("main-form");
